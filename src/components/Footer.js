268c76429155682f39b51783b1b9a3220ef986ab
Jakob Gøttrup Nielsen <jakobgoettrup@gmail.com>
2019-05-15 00:07:22 +0200

    team

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,18 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from 'react';
+import {Link} from 'gatsby';
 
-import logo from '../img/teal-darkbg.png'
-import facebook from '../img/social/facebook.svg'
-import instagram from '../img/social/instagram.svg'
-import twitter from '../img/social/twitter.svg'
-import vimeo from '../img/social/vimeo.svg'
+import logo from '../img/teal-darkbg.png';
+import facebook from '../img/social/facebook.svg';
+import instagram from '../img/social/instagram.svg';
+import twitter from '../img/social/twitter.svg';
+import vimeo from '../img/social/vimeo.svg';
 
 const Footer = class extends React.Component {
-  render() {
+  render () {
     return (
       <footer className="footer has-background-black has-text-white-ter">
         <div className="content has-text-centered">
-          <img
-            src={logo}
-            alt="Teal"
-            style={{ width: '10em', height: '4em' }}
-          />
+          <img src={logo} alt="Teal" style={{width: '10em', height: '4em'}} />
         </div>
         <div className="content has-text-centered has-background-black has-text-white-ter">
           <div className="container has-background-black has-text-white-ter">
@@ -55,6 +51,11 @@ const Footer = class extends React.Component {
               <div className="column is-4">
                 <section>
                   <ul className="menu-list">
+                    <li>
+                      <Link className="navbar-item" to="/team">
+                        Team
+                      </Link>
+                    </li>
                     <li>
                       <Link className="navbar-item" to="/blog">
                         Blog
@@ -103,8 +104,8 @@ const Footer = class extends React.Component {
           </div>
         </div>
       </footer>
-    )
+    );
   }
-}
+};
 
-export default Footer
+export default Footer;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -66,6 +66,9 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/products">
                 Produkter
               </Link>
+                            <Link className="navbar-item" to="/team">
+                Team
+              </Link>
               <Link className="navbar-item" to="/blog">
                 Blog
               </Link>
